9df6d06e34950e437f465023c8cc5e05b50f9a26
Jacqui Comstock <41028747+jacquicomstock@users.noreply.github.com>
2022-07-29 15:19:21 -0700

    Create 0_uploadprep.sh

---
+++ 0_uploadprep.sh
@@ -0,0 +1,3 @@
+#Upload files to cluster
+scp -r Elisa.zip carlsonlab@pod.cnsi.ucsb.edu:/home/carlsonlab/elisa/
+scp -r silva_nr99_v138.1_wSpecies_train_set.fa.gz carlsonlab@pod.cnsi.ucsb.edu:/home/carlsonlab/elisa/
