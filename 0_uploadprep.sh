4808bfc6b8f5ba9ba0f61b376998b3d1f9e5a964
Jacqui Comstock <41028747+jacquicomstock@users.noreply.github.com>
2022-07-29 15:38:06 -0700

    Update 0_uploadprep.sh

--- 0_uploadprep.sh
+++ 0_uploadprep.sh
@@ -1,3 +1,24 @@
 #Upload files to cluster
 scp -r Elisa.zip carlsonlab@pod.cnsi.ucsb.edu:/home/carlsonlab/elisa/
 scp -r silva_nr99_v138.1_wSpecies_train_set.fa.gz carlsonlab@pod.cnsi.ucsb.edu:/home/carlsonlab/elisa/
+
+#to unzip all .zip files
+unzip '*.zip'
+
+#make filtered directory for DADA2
+mkdir filtered
+
+#activate conda environment with R
+conda activate R4.2.0
+
+#submit batch job to slurm to run dada2 pipeline
+sbatch \
+	--job-name=Baratheon_dada2 \
+	--nodes=1 \
+	--tasks-per-node=32 \
+	--cpus-per-task=1 \
+	--mem=80G \
+	--time=4:00:00 \
+	--output=dada2_out \
+	--error=dada2_err \
+	--wrap="Rscript Baratheon_dada2.R"
